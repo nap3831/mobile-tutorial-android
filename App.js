7368a35ddca681b767ad97d1f8cbba1c070c9ef2
nap3831 <nap3831@gmail.com>
2020-04-09 00:36:15 +0700

    Changed Style inline to internal styles

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import {
-  View, Text, ScrollView
+  View, Text, ScrollView, StyleSheet
 } from 'react-native';
 
 class App extends Component {
@@ -12,8 +12,6 @@ class App extends Component {
     }
   }
   componentDidMount() {
-    console.log('hello');
-
     const url = "https://corona.lmao.ninja/countries";
     axios.get(url).then((Response) => {
       this.setState({
@@ -21,50 +19,74 @@ class App extends Component {
       })
     })
   }
+
   render() {
+    const StylePage = StyleSheet.create({
+      container: {
+        flex: 1, backgroundColor: '#333333'
+      },
+      header: {
+        width: '100%', color: '#ffff00', justifyContent: 'center', textAlign: 'center', fontSize: 25
+      },
+      boxContent:{
+        width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%', flex: 1 
+      },
+      underlineDetailContent:{
+        backgroundColor: '#ff0000', width: '50%', height: 1, marginTop: '5%', marginBottom: '5%'
+      },
+      underlineEndContent :{
+        backgroundColor: '#ffffff', width: '100%', height: 2, marginTop: '5%', marginBottom: '5%'
+      },
+      detail:{
+        textAlignVertical: 'center', color: '#ffffff', fontSize: 25, flexWrap: 'wrap'
+      }
+
+    });
     return (
-      <View style={{ flex: 1, backgroundColor: '#333333' }}>
+      <View style={StylePage.container}>
         <ScrollView>
           {this.state.dataCoronaVirus.map((datas, index) => {
-            return(
-               <View key={index} style={{ flexDirection: 'column', flex: 1 }}>
-                    <Text style={{ width: '100%', color: '#fff', justifyContent: 'center', textAlign: 'center', fontSize: 30 }}>
-                      ชื่อประเทศ
+            return (
+              <View key={index} >
+                <View style={StylePage.boxContent}>
+                <Text style={StylePage.header}>
+                  ชื่อประเทศ
                 </Text>
-                    <View style={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', marginBottom: 25, marginTop: 25, height: '100%', flex: 1 }}>
-                      <Text style={{ textAlignVertical: 'center', color: '#ffffff', fontSize: 25, marginTop: 25, flexWrap: 'wrap' }}>
-                        {datas.country}
-                      </Text>
-                      <View style={{ backgroundColor: '#ff0000', width: '100%', height: '3%', marginTop: '5%', marginBottom: '5%' }}></View>
-                      <Text style={{ width: '100%', color: '#fff', justifyContent: 'center', textAlign: 'center', fontSize: 30 }}>
-                        ผู้ติดเชื้อทั้งหมด
-                </Text>
-                      <Text style={{ textAlignVertical: 'center', color: '#ffffff', fontSize: 25, flexWrap: 'wrap' }}>
-                        {datas.cases} คน
+                
+                  <Text style={ StylePage.detail}>
+                    {datas.country}
                   </Text>
-                      <View style={{ backgroundColor: '#ff0000', width: '50%', height: '3%', marginTop: '5%', marginBottom: '5%' }}></View>
-                      <Text style={{ width: '100%', color: '#fff', justifyContent: 'center', textAlign: 'center', fontSize: 30 }}>
-                        ผู้เสียชีวิตแล้ว
+                  <View style={StylePage.underlineDetailContent}></View>
+                  <Text style={StylePage.header}>
+                    ผู้ติดเชื้อทั้งหมด
                 </Text>
-                      <Text style={{ textAlignVertical: 'center', color: '#ffffff', fontSize: 25, flexWrap: 'wrap' }}>
-                        {datas.deaths} คน
+                <View style={StylePage.underlineDetailContent}></View>
+                  <Text style={StylePage.detail}>
+                    {datas.cases} คน
                   </Text>
-                      <View style={{ backgroundColor: '#ff0000', width: '50%', height: '3%', marginTop: '5%', marginBottom: '5%' }}></View>
-                      <Text style={{ width: '100%', color: '#fff', justifyContent: 'center', textAlign: 'center', fontSize: 30 }}>
-                        ติดเชื้อเพิ่มวันนี้จำนวน
+                  <View style={StylePage.underlineDetailContent}></View>
+                  <Text style={StylePage.header}>
+                    ผู้เสียชีวิตแล้ว
                 </Text>
-                      <Text style={{ textAlignVertical: 'center', color: '#ffffff', fontSize: 25, flexWrap: 'wrap' }}>
-                        {datas.todayCases} คน
+                  <Text style={StylePage.detail}>
+                    {datas.deaths} คน
                   </Text>
-                      <View style={{ backgroundColor: '#ff0000', width: '50%', height: '3%', marginTop: '5%', marginBottom: '5%' }}></View>
-                      <View style={{ backgroundColor: '#ffffff', width: '100%', height: '3%', marginTop: '5%', marginBottom: '5%' }}></View>
-                    </View>
-                  </View>)
-              
+                  <View style={StylePage.underlineDetailContent}></View>
+                  <Text style={StylePage.header}>
+                    ติดเชื้อเพิ่มวันนี้จำนวน
+                </Text>
+                  <Text style={StylePage.detail}>
+                    {datas.todayCases} คน
+                  </Text>
+                  <View style={StylePage.underlineDetailContent}></View>
+                  <View style={StylePage.underlineEndContent}></View>
+                </View>
+              </View>)
+
           })}
         </ScrollView>
 
-      </View>
+      </View >
     )
   }
 }
